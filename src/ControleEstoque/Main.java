e54c8d450b97e9edfef212a011d4cd8f248d3a23
Arthur-Henrique-Student-UnC <arthur.henrique@aluno.unc.br>
2023-10-31 19:05:50 -0300

    teste

---
+++ src/ControleEstoque/Main.java
@@ -0,0 +1,20 @@
+package ControleEstoque;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int TamanhoEstoque = 100;
+        String[] produtos = new String[TamanhoEstoque];
+        int[] quantidades = new int[TamanhoEstoque];
+        int indice = 0;
+
+        boolean executando = true;
+
+    }
+    public static void adicionarProduto(String[] produtos, int [] quantidade, int indice; String produto, int quantidade)
+
+
+}
